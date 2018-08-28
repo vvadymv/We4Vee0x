6d25fa18955c8f4c74e284f63ad291f20b7bec68
vvadymv <38409513+vvadymv@users.noreply.github.com>
2018-08-28 11:41:30 +0300

    Update get-caller-identity.py

--- get-caller-identity.py
+++ get-caller-identity.py
@@ -3,8 +3,8 @@ import boto3
 
 # playtech.com
 
-access_key = 'AKIAICDFTTNOXYO2FJFA'
-secret_key = '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'
+access_key = ''
+secret_key = ''
 
 def get_aws_account_id(access_key, secret_key):
     sts = boto3.client(
